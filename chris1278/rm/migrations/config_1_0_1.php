7ceb9a87959693302f2297dbe6c0e0508a01da8b
chris1278 <31364765+chris1278@users.noreply.github.com>
2020-10-11 03:02:35 +0200

    Add files via upload

--- chris1278/rm/migrations/config_1_0_1.php
+++ chris1278/rm/migrations/config_1_0_1.php
@@ -12,20 +12,9 @@ namespace chris1278\rm\migrations;
 
 class config_1_0_1 extends \phpbb\db\migration\migration
 	{
-
 	static public function depends_on()
 	{
-		return array('\chris1278\rm\migrations\config_1_0_1');
-	}
-
-	public function effectively_installed()
-	{
-		return;
-	}
-
-	static public function depends_on()
-	{
-		return array('\phpbb\db\migration\data\v310\dev');
+		return array('\chris1278\rm\migrations\config_1_0_0');
 	}
 
 	public function update_data()
