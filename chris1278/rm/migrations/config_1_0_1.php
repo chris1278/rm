0a37f58841a1130cc92e34e55c32608f5c127d2f
chris1278 <31364765+chris1278@users.noreply.github.com>
2020-10-11 02:55:09 +0200

    Add files via upload

---
+++ chris1278/rm/migrations/config_1_0_1.php
@@ -0,0 +1,44 @@
+<?php
+/**
+*
+* Real-time statistics with Revolvermaps Extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2020 (Christian-Esch.de)
+* @license GNU General Public License, version 2 (GPL-2.0-only)
+*
+*/
+
+namespace chris1278\rm\migrations;
+
+class config_1_0_1 extends \phpbb\db\migration\migration
+	{
+
+	static public function depends_on()
+	{
+		return array('\chris1278\rm\migrations\config_1_0_1');
+	}
+
+	public function effectively_installed()
+	{
+		return;
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\dev');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.update', array('rm_version', '1.0.1')),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			array('config.remove', array('rm_version', '')),
+		);
+	}
+}
